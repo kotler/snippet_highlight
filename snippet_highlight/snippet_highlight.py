c1c4911e2b2a0a7ad60cc79aa738c287cbf0522d
Everett Kotler <kotler@gmail.com>
2014-02-21 10:18:59 +0900

    Refactored to organize code under a Snippet class.

--- snippet_highlight/snippet_highlight.py
+++ snippet_highlight/snippet_highlight.py
@@ -1,38 +1,116 @@
-HIGHLIGHT = '[[HIGHLIGHT]]'
-END_HIGHLIGHT = '[[ENDHIGHLIGHT]]'
+import string
 
 
 def highlight_doc(document, query):
-    if is_document_or_query_blank(document, query):
-        return document
+    snippet = Snippet(document, query)
+    return snippet.highlighted()
 
-    query_terms = query.split(' ')
 
-    document_terms = document.split(' ')
+class Snippet(object):
+    LENGTH = 256
+    HIGHLIGHT = '[[HIGHLIGHT]]'
+    END_HIGHLIGHT = '[[ENDHIGHLIGHT]]'
 
-    result_terms = []
-    to_highlight = []
-    for term in document_terms:
-        if term in query_terms:
-            to_highlight.append(term)
+    def __init__(self, full_text, query):
+        self.full_text = full_text
+        self.query = query
+        self.snippet = self._find_most_relevant_snippet()
+
+    def _find_most_relevant_snippet(self):
+        """
+        1.  Split document in sentences
+        2.  Rank each sentence (count of query terms, count of adjacent terms,
+            exact query matches)
+        3.  Start snippet preferably at the beginning of a sentence. If a
+            sentence is longer than window_size make sure snippet contains
+            a query term.
+        """
+        if self._is_full_text_or_query_blank():
+            return self.full_text[:self.LENGTH]
         else:
-            if len(to_highlight) > 0:
-                result_terms.append(highlight(' '.join(to_highlight)))
-                to_highlight = []
-            result_terms.append(term)
+            return self.full_text[:self.LENGTH]
 
-    if len(to_highlight) > 0:
-        result_terms.append(highlight(' '.join(to_highlight)))
+    def _is_full_text_or_query_blank(self):
+        if self.full_text is '' or self.query is '':
+            return True
+        else:
+            return False
 
-    return ' '.join(result_terms)
+    def highlighted(self):
+        if not self.query:
+            return self.snippet
+
+        query_terms = []
+        for term in self.query.split(' '):
+            query_terms.append(self._prep_term(term))
+
+        snippet_terms = self.snippet.split(' ')
+
+        result_terms = []
+        to_highlight = []
+        for term in snippet_terms:
+            if self._prep_term(term) in query_terms:
+                to_highlight.append(term)
+            else:
+                if len(to_highlight) > 0:
+                    result_terms.append(self._highlight(' '.join(
+                        to_highlight)))
+                    to_highlight = []
+                result_terms.append(term)
+
+        if len(to_highlight) > 0:
+            result_terms.append(self._highlight(' '.join(to_highlight)))
+
+        return ' '.join(result_terms)
+
+    def _highlight(self, term):
+        return (self.HIGHLIGHT + without_trailing_punctuation(term) +
+                self.END_HIGHLIGHT + trailing_punctuation(term))
+
+    def _prep_term(self, term):
+        return without_trailing_punctuation(term).lower()
 
 
-def is_document_or_query_blank(document, query):
-    if document is '' or query is '':
-        return True
-    else:
-        return False
+class Ranked_Sentence(object):
+    QUERY_TERMS_WEIGHT = 0.25
+    ADJACENT_QUERY_TERMS_WEIGHT = 0.25
+    EXACT_MATCH_WEIGHT = 0.5
+
+    def __init__(self, text, query):
+        self.text = text
+        self.rank = self.rank(query)
+
+    def rank(self):
+        rank = (self.count_of_query_terms() * self.QUERY_TERMS_WEIGHT +
+                self.count_of_adjacent_query_terms *
+                self.ADJACENT_QUERY_TERMS_WEIGHT +
+                self.count_of_exact_matches() * self.EXACT_MATCH_WEIGHT)
+        return rank
+
+    def count_of_query_terms(self):
+        return 0
+
+    def count_of_adjacent_query_terms(self):
+        return 0
+
+    def count_of_exact_matches(self):
+        return 0
 
 
-def highlight(term):
-    return HIGHLIGHT + term + END_HIGHLIGHT
+def without_trailing_punctuation(text):
+    if len(text) < 1:
+        return text
+    if text[len(text) - 1] in string.punctuation:
+        text = without_trailing_punctuation(text[:-1])
+    return text
+
+
+def trailing_punctuation(text):
+    result = ""
+    if len(text) < 1:
+        return result
+    position = len(text) - 1
+    while text[position] in string.punctuation:
+        result = text[position] + result
+        position -= 1
+    return result
--- tests/snippet_highlight_tests.py
+++ tests/snippet_highlight_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from nose.tools import *  # PEP8 asserts
-from snippet_highlight import snippet_highlight
+from snippet_highlight.snippet_highlight import *
 
 
 class SnippetHighlightTest(unittest.TestCase):
@@ -29,57 +29,62 @@ class SnippetHighlightTest(unittest.TestCase):
         return result
 
     def highlight(self, text):
-        return (snippet_highlight.HIGHLIGHT + text +
-                snippet_highlight.END_HIGHLIGHT)
+        return (Snippet.HIGHLIGHT + text +
+                Snippet.END_HIGHLIGHT)
 
     def test_highlight_doc_blank_doc_and_query(self):
-        assert_equal('', snippet_highlight.highlight_doc('', ''))
+        assert_equal('', highlight_doc('', ''))
 
     def test_highlight_doc_blank_query(self):
         assert_equal(self.basic_document,
-                     snippet_highlight.highlight_doc(self.basic_document, ''))
+                     highlight_doc(self.basic_document, ''))
 
     def test_highlight_doc_blank_doc(self):
         assert_equal('',
-                     snippet_highlight.highlight_doc('', self.basic_query))
+                     highlight_doc('', self.basic_query))
 
     def test_highlight_doc(self):
         assert_equal(self.basic_result,
-                     snippet_highlight.highlight_doc(self.basic_document,
-                                                     self.basic_query))
+                     highlight_doc(self.basic_document,
+                                   self.basic_query))
 
     def test_highlight_doc_multiple_words(self):
-        assert_equal(self.multi_word_result, snippet_highlight.highlight_doc(
+        assert_equal(self.multi_word_result, highlight_doc(
             self.basic_document, self.basic_document))
 
     def test_highlight_doc_longer_query_than_doc(self):
-        assert_equal(self.multi_word_result, snippet_highlight.highlight_doc(
+        assert_equal(self.multi_word_result, highlight_doc(
             self.basic_document, self.basic_document + " baz"))
 
     def test_highlight_doc_no_hit_query(self):
-        assert_equal(self.basic_document, snippet_highlight.highlight_doc(
+        assert_equal(self.basic_document, highlight_doc(
             self.basic_document, "baz"))
 
-    def test_is_document_or_query_blank(self):
-        assert_true(snippet_highlight.is_document_or_query_blank('', ''))
-        assert_true(snippet_highlight.is_document_or_query_blank('',
-                    self.basic_query))
-        assert_true(snippet_highlight.is_document_or_query_blank(
-            self.basic_document, ''))
-        assert_false(snippet_highlight.is_document_or_query_blank(
-            self.basic_document, self.basic_query))
+    # def test_is_document_or_query_blank(self):
+    #     assert_true(Snippet._is_full_text_or_query_blank('', ''))
+    #     assert_true(Snippet._is_full_text_or_query_blank('',
+    #                 self.basic_query))
+    #     assert_true(Snippet._is_full_text_or_query_blank(
+    #         self.basic_document, ''))
+    #     assert_false(Snippet._is_full_text_or_query_blank(
+    #         self.basic_document, self.basic_query))
 
     def test_highlight_doc_in_order_query_longer_doc(self):
-        assert_is_not(-1,
-                      snippet_highlight.highlight_doc(self.long_document,
-                                                      self.long_query).find(
-                          self.highlighted_long_query))
+        highlighted_doc = highlight_doc(self.long_document, self.long_query)
+        print highlighted_doc
+        assert_is_not(-1, highlighted_doc.find(self.highlighted_long_query))
 
     def test_highlight_doc_with_trailing_puctuation(self):
         doc = self.basic_document + ','
         result = self.basic_result + ','
-        assert_equal(snippet_highlight.highlight_doc(doc, self.basic_query),
-                     result)
+        assert_equal(result,
+                     highlight_doc(doc, self.basic_query))
+
+    def test_highlight_doc_case_sensitivity(self):
+        doc = self.basic_document.upper()
+        result = self.basic_result.upper()
+        assert_equal(result,
+                     highlight_doc(doc, self.basic_query))
 
 if __name__ == '__main__':
     unittest.main()
